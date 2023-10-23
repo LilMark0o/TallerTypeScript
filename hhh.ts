f8e0af6dc1b36b54b220bccf810ec8271d5e59cb
Marco Alejandro Ramírez <ma.ramirez23@uniandes.edu.co>
2023-10-22 21:49:18 -0500

    taller 2 completo

--- hhh.ts
+++ hhh.ts
@@ -2,6 +2,7 @@
 
 // Función para agregar una fila a la tabla
 import { series } from "./data.js";
+import { Serie } from "./index.js";
 
 const miBoton = document.getElementById("miBoton");
 if (miBoton) {
@@ -18,24 +19,58 @@ function miFuncion() {
     promedio = promedio / numPeliculas;
     document.getElementsByTagName("p")[0].innerHTML = "El promedio de temporadas es " + promedio;
 }
+function showCard(serie: Serie,) {
+    // Coloca aquí el código que deseas ejecutar cuando se hace clic en el botón
+    const card = document.querySelector(".card");
+
+    if (card) {
+        // Modifica el contenido de la tarjeta
+        const cardTitle = card.querySelector(".card-title");
+        const cardText = card.querySelector(".card-text");
+
+        if (cardTitle && cardText) {
+            cardTitle.textContent = serie.name;
+            cardText.textContent = serie.description;
+        }
+
+        // También puedes modificar la imagen de la tarjeta si es necesario
+        const cardImage = card.querySelector(".card-img-top") as HTMLImageElement;
+
+        if (cardImage) {
+            // Ahora puedes acceder a la propiedad 'src' de la imagen
+            cardImage.src = "images/" + serie.name + ".jpeg";
+            cardImage.alt = serie.name;
+        }
+
+        // Puedes cambiar el enlace y su texto
+        const cardLink = card.querySelector(".stretched-link") as HTMLAnchorElement;
+
+        if (cardLink) {
+            // Ahora puedes acceder a la propiedad 'href' del enlace
+            cardLink.href = serie.website;
+            cardLink.textContent = "Ir al sitio web";
+        }
+    }
+}
 // Función para agregar una fila a la tabla
-function agregarFila(id: number, nombre: string, canal: string, temporadas: number) {
+function agregarFila(serie: Serie) {
     const tbody = document.querySelector("tbody");
 
     if (tbody) {
         const fila = document.createElement("tr");
 
         const idColumna = document.createElement("td");
-        idColumna.textContent = id.toString();
+        idColumna.textContent = serie.id.toString();
 
         const nombreColumna = document.createElement("td");
-        nombreColumna.textContent = nombre;
+        nombreColumna.innerHTML = `<a class "text-info"  id="${serie.name}" target="_blank">${serie.name}</a>`
+        nombreColumna.addEventListener("click", () => { showCard(serie); })
 
         const canalColumna = document.createElement("td");
-        canalColumna.textContent = canal;
+        canalColumna.textContent = serie.channel;
 
         const temporadasColumna = document.createElement("td");
-        temporadasColumna.textContent = temporadas.toString();
+        temporadasColumna.textContent = serie.seasons.toString();
 
         fila.appendChild(idColumna);
         fila.appendChild(nombreColumna);
@@ -50,6 +85,6 @@ function agregarFila(id: number, nombre: string, canal: string, temporadas: numb
 
 // Itera sobre la lista de series y agrega filas a la tabla
 for (const serie of series) {
-    agregarFila(serie.id, serie.name, serie.channel, serie.seasons);
+    agregarFila(serie);
 }
 
--- scripts/hhh.js
+++ scripts/hhh.js
@@ -17,19 +17,47 @@ function miFuncion() {
     promedio = promedio / numPeliculas;
     document.getElementsByTagName("p")[0].innerHTML = "El promedio de temporadas es " + promedio;
 }
+function showCard(serie) {
+    // Coloca aquí el código que deseas ejecutar cuando se hace clic en el botón
+    var card = document.querySelector(".card");
+    if (card) {
+        // Modifica el contenido de la tarjeta
+        var cardTitle = card.querySelector(".card-title");
+        var cardText = card.querySelector(".card-text");
+        if (cardTitle && cardText) {
+            cardTitle.textContent = serie.name;
+            cardText.textContent = serie.description;
+        }
+        // También puedes modificar la imagen de la tarjeta si es necesario
+        var cardImage = card.querySelector(".card-img-top");
+        if (cardImage) {
+            // Ahora puedes acceder a la propiedad 'src' de la imagen
+            cardImage.src = "images/" + serie.name + ".jpeg";
+            cardImage.alt = serie.name;
+        }
+        // Puedes cambiar el enlace y su texto
+        var cardLink = card.querySelector(".stretched-link");
+        if (cardLink) {
+            // Ahora puedes acceder a la propiedad 'href' del enlace
+            cardLink.href = serie.website;
+            cardLink.textContent = "Ir al sitio web";
+        }
+    }
+}
 // Función para agregar una fila a la tabla
-function agregarFila(id, nombre, canal, temporadas) {
+function agregarFila(serie) {
     var tbody = document.querySelector("tbody");
     if (tbody) {
         var fila = document.createElement("tr");
         var idColumna = document.createElement("td");
-        idColumna.textContent = id.toString();
+        idColumna.textContent = serie.id.toString();
         var nombreColumna = document.createElement("td");
-        nombreColumna.textContent = nombre;
+        nombreColumna.innerHTML = "<a class \"text-info\"  id=\"".concat(serie.name, "\" target=\"_blank\">").concat(serie.name, "</a>");
+        nombreColumna.addEventListener("click", function () { showCard(serie); });
         var canalColumna = document.createElement("td");
-        canalColumna.textContent = canal;
+        canalColumna.textContent = serie.channel;
         var temporadasColumna = document.createElement("td");
-        temporadasColumna.textContent = temporadas.toString();
+        temporadasColumna.textContent = serie.seasons.toString();
         fila.appendChild(idColumna);
         fila.appendChild(nombreColumna);
         fila.appendChild(canalColumna);
@@ -43,5 +71,5 @@ function agregarFila(id, nombre, canal, temporadas) {
 // Itera sobre la lista de series y agrega filas a la tabla
 for (var _i = 0, series_1 = series; _i < series_1.length; _i++) {
     var serie = series_1[_i];
-    agregarFila(serie.id, serie.name, serie.channel, serie.seasons);
+    agregarFila(serie);
 }
